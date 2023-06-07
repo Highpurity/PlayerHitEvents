1d0200683f23ba72787dc12bcf02461c00b4a344
Highpurity <development@highpurity.cc>
2023-06-07 00:07:34 -0400

    Progress!

--- src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
+++ src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
@@ -1,6 +1,7 @@
 package highpurity.playerhitevents.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -9,9 +10,13 @@ public class PlayerPlayerEvent implements Listener {
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent e) {
 
-        var damager = e.getDamager().toString();
+        if(e.getDamager().getType() == EntityType.PLAYER){
+            if(e.getEntity().getType() == EntityType.PLAYER){
+                Bukkit.broadcastMessage("test");
+            }
+        }
 
-        Bukkit.broadcastMessage(damager);
+
 
     }
 }
