6497dd8aae6ed68cdf9df13be368a797f7fe0e21
Highpurity <development@highpurity.cc>
2023-06-07 00:31:25 -0400

    Incomplete PvE

--- src/main/java/highpurity/playerhitevents/events/PlayerMobEvent.java
+++ src/main/java/highpurity/playerhitevents/events/PlayerMobEvent.java
@@ -1,5 +1,8 @@
 package highpurity.playerhitevents.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -8,7 +11,15 @@ public class PlayerMobEvent implements Listener {
 
     @EventHandler
     public void onPlayerMobEvent(EntityDamageByEntityEvent e){
-        if()
+        if(e.getDamager().getType() == EntityType.PLAYER){
+            if(!e.getEntity().isDead()){
+                var entity = e.getEntity();
+                var playername = e.getDamager().getName();
+                var player = e.getDamager();
+
+                Bukkit.broadcastMessage(ChatColor.GREEN + playername + ChatColor.WHITE + ChatColor.RED +"❤");
+            }
+        }
     }
 
 }
