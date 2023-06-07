43195749c660d124cb93210a676c2d4e8f9c4d20
Highpurity <development@highpurity.cc>
2023-06-06 23:56:46 -0400

    added reload command with scanner

--- src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
+++ src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
@@ -1,11 +1,12 @@
 package highpurity.playerhitevents.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 public class PlayerPlayerEvent implements Listener {
 
-    @Override
+    @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent e) {
 
         var damager = e.getDamager().toString();
