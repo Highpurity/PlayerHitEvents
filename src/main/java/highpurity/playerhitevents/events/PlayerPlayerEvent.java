9aba9739db406c0e08750b7e28340f26b82c1e88
Highpurity <development@highpurity.cc>
2023-06-08 17:48:16 -0400

    Got correct vars

--- src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
+++ src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
@@ -1,7 +1,6 @@
 package highpurity.playerhitevents.events;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
@@ -17,10 +16,15 @@ public class PlayerPlayerEvent implements Listener {
                 if(e.getEntity().getType() == EntityType.PLAYER){
 
                     LivingEntity damager = (LivingEntity) e.getDamager();
+                    LivingEntity entity = (LivingEntity) e.getEntity();
 
-                    damager.getHealth();
+                    var damhealth = damager.getHealth();
+                    var dammax = damager.getMaxHealth();
 
-                    Bukkit.broadcastMessage("test");
+                    var enthealth = entity.getHealth();
+                    var entmax = entity.getMaxHealth();
+
+                    Bukkit.broadcastMessage("");
         }
 
             }
