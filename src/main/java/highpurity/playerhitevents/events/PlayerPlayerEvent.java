95ffc2f65d113b2f0cac36c452def694322113fe
Highpurity <development@highpurity.cc>
2023-06-08 17:44:42 -0400

    Found livingentity fix

--- src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
+++ src/main/java/highpurity/playerhitevents/events/PlayerPlayerEvent.java
@@ -1,7 +1,9 @@
 package highpurity.playerhitevents.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -10,12 +12,17 @@ public class PlayerPlayerEvent implements Listener {
     @EventHandler
     public void onEntityDamage(EntityDamageByEntityEvent e) {
 
-        if(e.getDamager().getType() == EntityType.PLAYER){
-            if(e.getEntity().getType() == EntityType.PLAYER){
+        if(e.getEntity() instanceof LivingEntity){
+            if(e.getDamager().getType() == EntityType.PLAYER){
+                if(e.getEntity().getType() == EntityType.PLAYER){
 
+                    LivingEntity damager = (LivingEntity) e.getDamager();
 
+                    damager.getHealth();
 
-                Bukkit.broadcastMessage("test");
+                    Bukkit.broadcastMessage("test");
+        }
+
             }
         }
 
